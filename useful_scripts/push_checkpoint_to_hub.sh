67cd42c64e335ef0115ef44acb74b1848adeaae4
Stefano Berti <sberti@iiticubws091u.iit.local>
2025-09-25 10:14:17 +0200

    Latest training and eval scripts

---
+++ useful_scripts/push_checkpoint_to_hub.sh
@@ -0,0 +1,4 @@
+/home/sberti/.conda/envs/lerobot/bin/huggingface-cli upload \
+        --private \
+        steb6/trained-ergocub-pick-plush-act \
+        /fastwork/sberti/lerobot/checkpoints/ergocub-pick-plush-act/checkpoints/100000/pretrained_model
