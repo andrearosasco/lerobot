3329ec069b6b87bd65735be98b06f3072e895963
Andrea Rosasco <andrearosasco.ar@gmail.com>
2025-10-08 16:33:52 +0200

    ergocup-point data collection and training

--- src/lerobot/record.py
+++ src/lerobot/record.py
@@ -75,9 +75,6 @@ python -m lerobot.record \
 ```
 """
 
-# import debugpy
-# debugpy.listen(("localhost", 5678))
-# debugpy.wait_for_client()
 
 import logging
 import time
--- useful_scripts/record.sh
+++ useful_scripts/record.sh
@@ -3,6 +3,7 @@ python -m lerobot.record \
    --robot.type=ergocub \
    --robot.remote_prefix="/ergocubSim" \
    --robot.local_prefix="/lerobot" \
+<<<<<<< Updated upstream
    --robot.cameras='{ external: {"type": "yarp", "yarp_name": "camera", "width": 640, "height": 480, "fps": 30}, egocentric:{"type": "yarp", "yarp_name": "ergocubSim/depthCamera/rgbImage:o", "width": 640, "height": 480, "fps": 30}}' \
    --robot.control_boards='[neck,left_arm,right_arm,fingers]' \
    --teleop.type=metaquest \
@@ -12,3 +13,15 @@ python -m lerobot.record \
    --dataset.reset_time=1 \
    --dataset.episode_time=8 \
    --dataset.single_task="get the ball"
+=======
+   --robot.cameras='{ agentview: {"type": "yarp", "yarp_name": "camera", "width": 640, "height": 480, "fps": 30}}' \
+   --robot.control_boards='[neck,left_arm,right_arm,fingers]' \
+   --teleop.type=metaquest \
+   --dataset.repo_id=ar0s/ergocub-point \
+   --dataset.num_episodes=46 \
+   --dataset.fps=10 \
+   --dataset.reset_time=2 \
+   --dataset.episode_time=10 \
+   --dataset.single_task="Point to target" \
+   --resume=True
+>>>>>>> Stashed changes
