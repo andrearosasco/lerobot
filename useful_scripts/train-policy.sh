c4073740c0193b8acfb1f782d113347614616926
Stefano Berti <berti.ste@hhotmail.it>
2025-11-27 17:06:07 +0100

    To train DP conditioned

--- useful_scripts/train-policy.sh
+++ useful_scripts/train-policy.sh
@@ -4,13 +4,13 @@ python \
 	--batch_size=8 \
 	--num_workers=4 \
 	--policy.type=diffusion \
-	--policy.repo_id=steb6/dp_redball_sim_reactive \
+	--policy.repo_id=steb6/dp_hri_ste_carmela_split \
 	--policy.crop_shape=null \
 	--policy.resize_shape=[240,320] \
 	--policy.noise_scheduler_type=DDIM \
 	--policy.num_train_timesteps=100 \
 	--policy.use_language_conditioning=true \
 	--policy.language_dropout=0.0 \
-	--dataset.repo_id=steb6/redball_sim_reactive \
+	--dataset.repo_id=steb6/hri_ste_carmela_split \
 	--policy.device=cuda \
-	--output_dir=checkpoints/redball_sim_reactive
+	--output_dir=checkpoints/hri_ste_carmela_split
