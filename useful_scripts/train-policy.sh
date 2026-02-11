3c982db43efab39a013d4622421e3348754d3193
Stefano Berti <berti.ste@hhotmail.it>
2026-02-11 14:04:23 +0100

    Removed default GC implelentationg

--- useful_scripts/train-policy.sh
+++ useful_scripts/train-policy.sh
@@ -62,8 +62,8 @@ CMD=(
                                        "contrast": {"weight": 1.0, "type": "ColorJitter", "kwargs": {"contrast": [0.8, 1.2]}}, 
                                        "saturation": {"weight": 1.0, "type": "ColorJitter", "kwargs": {"saturation": [0.5, 1.5]}}, 
                                        "hue": {"weight": 1.0, "type": "ColorJitter", "kwargs": {"hue": [-0.05, 0.05]}}, 
-                                       "sharpness": {"weight": 1.0, "type": "SharpnessJitter", "kwargs": {"sharpness": [0.5, 1.5]}}, 
-                                       "green_screen_replace": {"weight": -1.0, "type": "GreenScreenReplace", "kwargs": {"pool_dir": "./gs_image_pool", "green_ratio": 0.001, "min_green": 0.001, "spill": 0.001, "hue_min": 0.1, "hue_max": 0.50, "min_s": 0.10, "min_v": 0.10}}}'''
+                                       "sharpness": {"weight": 1.0, "type": "SharpnessJitter", "kwargs": {"sharpness": [0.5, 1.5]}}}'''
+                                    #    "green_screen_replace": {"weight": -1.0, "type": "GreenScreenReplace", "kwargs": {"pool_dir": "./gs_image_pool", "green_ratio": 0.001, "min_green": 0.001, "spill": 0.001, "hue_min": 0.1, "hue_max": 0.50, "min_s": 0.10, "min_v": 0.10}}}'''
 )
 
 # Add policy-specific parameters
